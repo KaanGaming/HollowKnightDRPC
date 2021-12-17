a3b4e6ed5f9a4355efef709b71d5f947c215ecdd
KaanGaming <kaangaming.gamejolt@gmail.com>
2021-12-17 19:57:15 +0300

    Improve README and other changes

---
+++ HollowKnightDRPC/BossTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Modding;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+namespace HollowKnightDRPC
+{
+    public partial class HollowKnightDRPC
+    {
+        float bossTimer = 0.0f;
+        GameObject knight = null;
+        
+        public void TimerUpdate()
+        {
+            if (bosses.Count > 0)
+            {
+                bossTimer += Time.deltaTime;
+                if (knight == null) GameObject.Find("Knight");
+                PlayMakerFSM roarLock = null;
+                foreach (PlayMakerFSM fsm in knight.GetComponents<PlayMakerFSM>())
+                {
+                    if (fsm.FsmName == "Roar Lock")
+                    {
+                        roarLock = fsm;
+                        break;
+                    }
+                }
+                if (roarLock == null) return;
+                
+            }
+        }
+    }
+}
